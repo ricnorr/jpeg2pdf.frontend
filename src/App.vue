2bbf2381a16e46363b5098727b88f2e87ea79365
ricnorr <kolyan125125@yandex.ru>
2021-01-23 21:32:56 +0300

    fdfdsfds

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,14 @@ export default {
       console.log(formData.get("file"));
       axios.post("https://jpeg2pdf.herokuapp.com/convertFile", formData)
           .then(
-              () => {
+              (response) => {
                 this.fileIsReady = true;
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', 'file.pdf'); //or any other extension
+                document.body.appendChild(link);
+                link.click();
               }
           ).catch((error) => console.log(error));
     },
